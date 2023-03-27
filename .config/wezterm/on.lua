3b2c2eb3a9b028e4b4d27be3eba78d0e3bd45598
aomoji <ixia.laevigata@gmail.com>
2023-03-27 11:44:24 +0900

    [modify] formatting active workspace names

--- .config/wezterm/on.lua
+++ .config/wezterm/on.lua
@@ -1,5 +1,9 @@
 local wezterm = require("wezterm")
 
 wezterm.on("update-right-status", function(window)
-  window:set_right_status(window:active_workspace())
+  window:set_right_status(
+    wezterm.format({
+      { Text = window:active_workspace() .. "    " },
+    })
+  )
 end)
