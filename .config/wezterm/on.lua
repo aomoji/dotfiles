c3515cf463d8e79d65750e2b7139763cdea63df2
aomoji <ixia.laevigata@gmail.com>
2023-03-27 12:18:25 +0900

    [modify] add default workspaces

--- .config/wezterm/on.lua
+++ .config/wezterm/on.lua
@@ -1,9 +1,20 @@
 local wezterm = require("wezterm")
+local mux = wezterm.mux
 
-wezterm.on("update-right-status", function(window)
-  window:set_right_status(
-    wezterm.format({
-      { Text = window:active_workspace() .. "    " },
-    })
-  )
+wezterm.on("update-right-status", function(window, domain)
+  window:set_right_status(wezterm.format({
+    { Text = window:active_workspace() .. "    " },
+  }))
+end)
+
+wezterm.on("gui-startup", function()
+  local tab, pane, window = mux.spawn_window({
+    workspace = "workspace 1",
+  })
+  local tab, pane, window = mux.spawn_window({
+    workspace = "workspace 2",
+  })
+  local tab, pane, window = mux.spawn_window({
+    workspace = "workspace 3",
+  })
 end)
