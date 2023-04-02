f927c61c3593fc33a5e726ad2c852fabd154155d
aomoji <ixia.laevigata@gmail.com>
2023-04-02 18:34:42 +0900

    [modify] added installation of wezterm and zinit.

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+brew install --cask wezterm
 brew install sk
 brew install lazygit
+bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
