de2f4cbe6198d1f2b05a538c8dc90345842e6b53
aomoji <ixia.laevigata@gmail.com>
2023-06-01 22:49:58 +0900

    [add] disabled opening hyperlink by simple left click

---
+++ .config/wezterm/mousebinds.lua
@@ -0,0 +1,31 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+local act = wezterm.action
+
+local M = {}
+
+M.mouse_bindings = {
+	-- Change the default click behavior so that it only selects
+	-- text and doesn't open hyperlinks
+	{
+		event = { Up = { streak = 1, button = "Left" } },
+		mods = "NONE",
+		action = act.CompleteSelection("PrimarySelection"),
+	},
+
+	-- and make CTRL-Click open hyperlinks
+	{
+		event = { Up = { streak = 1, button = "Left" } },
+		mods = "CTRL",
+		action = act.OpenLinkAtMouseCursor,
+	},
+
+	-- Disable the 'Down' event of CTRL-Click to avoid weird program behaviors
+	{
+		event = { Down = { streak = 1, button = "Left" } },
+		mods = "CTRL",
+		action = act.Nop,
+	},
+}
+
+return M
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,6 +1,7 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
 local keybinds = require("keybinds")
+local mousebinds = require("mousebinds")
 require("on")
 
 -- This table will hold the configuration.
@@ -9,7 +10,7 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 -- This is where you actually apply your config choices
@@ -39,14 +40,19 @@ config.tab_bar_at_bottom = true
 ---------------------------------------------------------------
 -- leader key
 config.leader = {
-  key = "q",
-  mods = "CTRL",
-  timeout_milliseconds = 2000,
+	key = "q",
+	mods = "CTRL",
+	timeout_milliseconds = 2000,
 }
 -- Set keybinds.
 config.keys = keybinds.create_keybinds()
 config.key_tables = keybinds.key_tables
 ---------------------------------------------------------------
+
+-----------------------------------------------------------------
+--- mouse bindings
+---------------------------------------------------------------
+config.mouse_bindings = mousebinds.mouse_bindings
 
 -- and finally, return the configuration to wezterm
 return config
