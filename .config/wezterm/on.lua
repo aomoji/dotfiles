a8edb1cf47707756e10ece46889ae98676187b8b
aomoji <ixia.laevigata@gmail.com>
2023-06-08 09:09:51 +0900

    [modify] enabled to edit tab title

--- .config/wezterm/on.lua
+++ .config/wezterm/on.lua
@@ -2,42 +2,55 @@ local wezterm = require("wezterm")
 local mux = wezterm.mux
 
 wezterm.on("update-right-status", function(window, domain)
-  window:set_right_status(wezterm.format({
-    { Text = window:active_workspace() .. "    " },
-  }))
+	window:set_right_status(wezterm.format({
+		{ Text = window:active_workspace() .. "    " },
+	}))
 end)
 
 wezterm.on("gui-startup", function()
-  local tab, pane, window = mux.spawn_window({
-    workspace = "workspace 1",
-  })
-  local tab, pane, window = mux.spawn_window({
-    workspace = "workspace 2",
-  })
-  local tab, pane, window = mux.spawn_window({
-    workspace = "workspace 3",
-  })
+	local tab, pane, window = mux.spawn_window({
+		workspace = "workspace 1",
+	})
+	local tab, pane, window = mux.spawn_window({
+		workspace = "workspace 2",
+	})
+	local tab, pane, window = mux.spawn_window({
+		workspace = "workspace 3",
+	})
 end)
 
+function tab_title(tab_info)
+	local title = tab_info.tab_title
+	-- if the tab title is explicitly set, take that
+	if title and #title > 0 then
+		return title
+	end
+	-- Otherwise, use the title from the active pane
+	-- in that tab
+	return tab_info.active_pane.title
+end
+
 wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
-  if tab.is_active then
-    return {
-      { Background = { Color = "navy" } },
-      { Text = " " .. tab.active_pane.title .. " " .. " - (" .. tab.active_pane.domain_name .. ")" },
-    }
-  end
-  local has_unseen_output = false
-  for _, pane in ipairs(tab.panes) do
-    if pane.has_unseen_output then
-      has_unseen_output = true
-      break
-    end
-  end
-  if has_unseen_output then
-    return {
-      { Background = { Color = "maroon" } },
-      { Text = " " .. tab.active_pane.title },
-    }
-  end
-  return tab.active_pane.title
+	if tab.is_active then
+		local title = tab_title(tab)
+		return {
+			{ Background = { Color = "navy" } },
+			{ Text = " " .. title .. " " .. " - (" .. tab.active_pane.domain_name .. ")" },
+		}
+	end
+	local has_unseen_output = false
+	for _, pane in ipairs(tab.panes) do
+		if pane.has_unseen_output then
+			has_unseen_output = true
+			break
+		end
+	end
+	if has_unseen_output then
+		local title = tab_title(tab)
+		return {
+			{ Background = { Color = "maroon" } },
+			{ Text = " " .. title },
+		}
+	end
+	return tab_title(tab)
 end)
