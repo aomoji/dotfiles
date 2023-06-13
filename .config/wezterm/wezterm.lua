3eac3a6dc4c5ef68b191b7a0bcb82a0cef487d00
aomoji <ixia.laevigata@gmail.com>
2023-06-13 23:43:14 +0900

    [modify] blinking cursor

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -10,7 +10,7 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 -- This is where you actually apply your config choices
@@ -26,7 +26,7 @@ config.color_scheme = "tokyonight_night"
 --- font
 ---------------------------------------------------------------
 -- For example, changing the color scheme:
-config.font = wezterm.font "Cica"
+config.font = wezterm.font("Cica")
 config.font_size = 14.0
 ---------------------------------------------------------------
 
@@ -41,6 +41,8 @@ config.enable_scroll_bar = true
 config.hide_tab_bar_if_only_one_tab = true
 -- Tab bar at bottom.
 config.tab_bar_at_bottom = true
+-- blinking cursor
+config.default_cursor_style = "BlinkingBlock"
 -----------------------------------------------------------------
 
 -----------------------------------------------------------------
@@ -48,9 +50,9 @@ config.tab_bar_at_bottom = true
 ---------------------------------------------------------------
 -- leader key
 config.leader = {
-  key = "q",
-  mods = "CTRL",
-  timeout_milliseconds = 2000,
+	key = "q",
+	mods = "CTRL",
+	timeout_milliseconds = 2000,
 }
 -- Set keybinds.
 config.keys = keybinds.create_keybinds()
@@ -67,9 +69,9 @@ config.mouse_bindings = mousebinds.mouse_bindings
 --- unix domain
 ---------------------------------------------------------------
 config.unix_domains = {
-  {
-    name = "unix",
-  },
+	{
+		name = "unix",
+	},
 }
 
 -- This causes `wezterm` to act as though it was started as
