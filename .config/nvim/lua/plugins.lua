5c23c01533e372ad513fae12930ff73d526038c0
aomoji <ixia.laevigata@gmail.com>
2023-07-24 23:55:57 +0900

    [wip] change formatter plugin

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -450,27 +450,76 @@ return require("packer").startup(function()
     end,
   })
 
-  use({
-    "jose-elias-alvarez/null-ls.nvim",
+  use {
+    'mhartington/formatter.nvim',
     config = function()
-      require("null-ls").setup({
-        -- debug = true,
-        capabilities = capabilities,
-        sources = {
-          require("null-ls").builtins.diagnostics.yamllint,
-          require("null-ls").builtins.formatting.rustfmt,
-          require("null-ls").builtins.formatting.stylua,
-          require("null-ls").builtins.formatting.shfmt,
-          require("null-ls").builtins.formatting.yamlfmt,
-          require("null-ls").builtins.formatting.sqlfluff.with({ extra_args = { "--dialect", "snowflake" } }),
-          require("null-ls").builtins.diagnostics.sqlfluff.with({
-            extra_args = { "--dialect", "snowflake" },
-          }),
-          require("null-ls").builtins.formatting.black,
-        },
-      })
-    end,
-  })
+      -- Utilities for creating configurations
+      local util = require "formatter.util"
+
+      -- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
+      require("formatter").setup {
+        -- Enable or disable logging
+        logging = true,
+        -- Set the log level
+        log_level = vim.log.levels.WARN,
+        -- All formatter configurations are opt-in
+        filetype = {
+          -- Formatter configurations for filetype "lua" go here
+          -- and will be executed in order
+          lua = {
+            -- "formatter.filetypes.lua" defines default configurations for the
+            -- "lua" filetype
+            require("formatter.filetypes.lua").stylua,
+          },
+          yaml = { require("formatter.filetypes.yaml").yamlfmt },
+          rust = { require("formatter.filetypes.rust").rustfmt },
+          sh = { require("formatter.filetypes.sh").shfmt },
+          sql = { 
+            require("formatter.filetypes.sql").sqlfmt,
+            function()
+              return {
+                exe = "sqlfmt",
+                args = {
+                  "-"
+                },
+                stdin = true,
+              }
+            end
+          },
+          python = { require("formatter.filetypes.python").black },
+          -- Use the special "*" filetype for defining formatter configurations on
+          -- any filetype
+          ["*"] = {
+            -- "formatter.filetypes.any" defines default configurations for any
+            -- filetype
+            require("formatter.filetypes.any").remove_trailing_whitespace
+          },
+        }
+      }
+    end
+  }
+
+  -- use({
+  --   "jose-elias-alvarez/null-ls.nvim",
+  --   config = function()
+  --     require("null-ls").setup({
+  --       -- debug = true,
+  --       capabilities = capabilities,
+  --       sources = {
+  --         require("null-ls").builtins.diagnostics.yamllint,
+  --         require("null-ls").builtins.formatting.rustfmt,
+  --         require("null-ls").builtins.formatting.stylua,
+  --         require("null-ls").builtins.formatting.shfmt,
+  --         require("null-ls").builtins.formatting.yamlfmt,
+  --         require("null-ls").builtins.formatting.sqlfluff.with({ extra_args = { "--dialect", "snowflake" } }),
+  --         require("null-ls").builtins.diagnostics.sqlfluff.with({
+  --           extra_args = { "--dialect", "snowflake" },
+  --         }),
+  --         require("null-ls").builtins.formatting.black,
+  --       },
+  --     })
+  --   end,
+  -- })
 
   use({
     "hrsh7th/nvim-cmp",
