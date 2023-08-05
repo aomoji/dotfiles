9768e8476e0438a0dd0c77843daea66fb4a7919a
aomoji <ixia.laevigata@gmail.com>
2023-08-06 00:08:44 +0900

    [modify] changed nvim's colorscheme

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -137,9 +137,9 @@ return require("packer").startup(function()
 		config = function()
 			require("rose-pine").setup({
 				--- @usage 'auto'|'main'|'moon'|'dawn'
-				variant = "main",
+				variant = "moon",
 				--- @usage 'main'|'moon'|'dawn'
-				dark_variant = "main",
+				dark_variant = "moon",
 			})
 			vim.cmd("colorscheme rose-pine")
 		end,
