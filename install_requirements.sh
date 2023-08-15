1c879c8c9abbd5ee9f8d4ae3996e1bf8524bce92
Kaito Oshiro <k.oshiro@churadata.okinawa>
2023-08-15 13:25:41 +0900

    [modfy] added node(npm) installation

--- install_requirements.sh
+++ install_requirements.sh
@@ -6,6 +6,7 @@ brew install --cask wezterm
 brew install sk
 brew install lazygit
 brew install direnv
+brew install node
 bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
