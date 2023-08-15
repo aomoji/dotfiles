72347f27f1361a6295de6ee99d33ec39d3a25b6a
Kaito Oshiro <k.oshiro@churadata.okinawa>
2023-08-15 13:09:58 +0900

    [modify] added mkdir command

--- create_link.sh
+++ create_link.sh
@@ -4,9 +4,12 @@ mkdir -p ~/.config/
 
 CURRENT_DIR=$(cd $(dirname $0); pwd)
 
+mkdir -p ~/.config/nvim
+mkdir -p ~/.config/nvim/lua
 ln -s $CURRENT_DIR/.config/nvim/init.vim ~/.config/nvim/init.vim
 ln -s $CURRENT_DIR/.config/nvim/lua/plugin.lua ~/.config/nvim/lua/plugin.lua
 
+mkdir -p ~/.config/wezterm
 ln -s $CURRENT_DIR/.config/wezterm/keybinds.lua ~/.config/wezterm/keybinds.lua
 ln -s $CURRENT_DIR/.config/wezterm/mousebinds.lua ~/.config/wezterm/mousebinds.lua
 ln -s $CURRENT_DIR/.config/wezterm/on.lua ~/.config/wezterm/on.lua
