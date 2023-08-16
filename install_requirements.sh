f5d632c7b41f8e58edec69f002054924e25d3035
Kaito Oshiro <k.oshiro@churadata.okinawa>
2023-08-16 18:46:51 +0900

    [modify] addedd poetry installation

--- install_requirements.sh
+++ install_requirements.sh
@@ -10,6 +10,7 @@ brew install direnv
 brew install node
 brew install luarocks
 brew install pyenv
+brew install poetry
 bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
