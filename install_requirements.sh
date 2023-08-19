fda70d5e6c70495094324d915fef8dab7449930d
aomoji <ixia.laevigata@gmail.com>
2023-08-19 12:59:41 +0900

    [modify] deleted packer installation

--- install_requirements.sh
+++ install_requirements.sh
@@ -12,6 +12,4 @@ brew install luarocks
 brew install pyenv
 brew install poetry
 bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 curl -sS https://starship.rs/install.sh | sh
