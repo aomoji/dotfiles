9e49df1fd4a244ca6e093d06701ebb89e9de42d1
Kaito Oshiro <k.oshiro@churadata.okinawa>
2023-08-21 16:32:54 +0900

    [add] telescope-live-grep-args.nvim

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,7 +1,13 @@
 return {
 	"nvim-telescope/telescope.nvim",
-	dependencies = { { "nvim-lua/popup.nvim" }, { "nvim-lua/plenary.nvim" }, { "mfussenegger/nvim-dap" } },
+	dependencies = {
+		{ "nvim-lua/popup.nvim" },
+		{ "nvim-lua/plenary.nvim" },
+		{ "mfussenegger/nvim-dap" },
+		{ "nvim-telescope/telescope-live-grep-args.nvim" },
+	},
 	config = function()
+		local lga_actions = require("telescope-live-grep-args.actions")
 		require("telescope").setup({
 			defaults = {
 				file_ignore_patterns = { ".git/" },
@@ -13,9 +19,35 @@ return {
 					end,
 				},
 			},
+			extensions = {
+				live_grep_args = {
+					auto_quoting = true, -- enable/disable auto-quoting
+					-- define mappings, e.g.
+					mappings = { -- extend mappings
+						i = {
+							["<C-k>"] = lga_actions.quote_prompt(),
+							["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+						},
+					},
+					-- ... also accepts theme settings, for example:
+					-- theme = "dropdown", -- use dropdown theme
+					-- theme = { }, -- use own theme spec
+					-- layout_config = { mirror=true }, -- mirror preview pane
+					--
+					-- (memo) use "-." for searching hidden files
+				},
+			},
 		})
+		-- live_grep_args
+		require("telescope").load_extension("live_grep_args")
+		-- keymaps
 		vim.api.nvim_set_keymap("n", "<leader>ff", "<Cmd>Telescope find_files hidden=true<CR>", { noremap = true })
-		vim.api.nvim_set_keymap("n", "<leader>fg", "<Cmd>Telescope live_grep<CR>", { noremap = true })
+		vim.api.nvim_set_keymap(
+			"n",
+			"<leader>fg",
+			":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>",
+			{ noremap = true }
+		)
 		vim.api.nvim_set_keymap("n", "<leader>fo", "<Cmd>Telescope oldfiles<CR>", { noremap = true })
 		vim.api.nvim_set_keymap("n", "<leader>fb", "<Cmd>Telescope buffers<CR>", { noremap = true })
 		vim.api.nvim_set_keymap("n", "<leader>he", "<Cmd>Telescope help_tags<CR>", { noremap = true })
