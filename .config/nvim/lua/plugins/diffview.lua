2bd488f1c9d39ea823fc5a432c58284c92c7659b
Kaito Oshiro <k.oshiro@churadata.okinawa>
2023-08-22 18:28:23 +0900

    [add] DiffviewOpen example

--- .config/nvim/lua/plugins/diffview.lua
+++ .config/nvim/lua/plugins/diffview.lua
@@ -2,6 +2,7 @@ return {
 	"sindrets/diffview.nvim",
 	dependencies = "nvim-lua/plenary.nvim",
 	-- DiffviewOpen
+	-- DiffviewOpen commit_a..commit_b -- path_to_file
 	-- DiffviewClose
 	-- DiffviewFileHistory
 }
