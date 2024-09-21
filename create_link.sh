0cb62f0c83fe36258420c40c0394e3ab04d39a3e
aomoji <ixia.laevigata@gmail.com>
2024-09-21 14:21:41 +0900

    [fix] make making link easy

--- create_link.sh
+++ create_link.sh
@@ -1,29 +1,35 @@
-#!/bin/sh
+#!/bin/bash
+
+function make_link() {
+    src=$1
+    dst=$2
+
+    if [[ -e $dst && ! -L $dst ]]; then
+        echo "$dst is not a symbolic link"
+        return 1
+    fi
+
+    if [[ ! -e $(dirname $dst) ]]; then
+        mkdir -p $(dirname $dst)
+    fi
+
+    if [[ -f $src ]]; then
+        ln -sf $src $dst
+    elif [[ -d $src ]]; then
+        ln -nsf $src $dst
+    fi
+}
 
 mkdir -p ~/.config/
 
-CURRENT_DIR=$(cd $(dirname $0); pwd)
+CURRENT_DIR=$(
+    cd $(dirname $0)
+    pwd
+)
 
-mkdir -p ~/.config/nvim
-mkdir -p ~/.config/nvim/lua
-ln -s $CURRENT_DIR/.config/nvim/init.lua ~/.config/nvim/init.lua
-ln -s $CURRENT_DIR/.config/nvim/lua/plugins.lua ~/.config/nvim/lua/plugins.lua
-ln -s $CURRENT_DIR/.config/nvim/lua/plugins ~/.config/nvim/lua/plugins
-ln -s $CURRENT_DIR/.config/nvim/lua/keymaps.lua ~/.config/nvim/lua/keymaps.lua
-ln -s $CURRENT_DIR/.config/nvim/lua/lazyvim.lua ~/.config/nvim/lua/lazyvim.lua
-ln -s $CURRENT_DIR/.config/nvim/lua/options.lua ~/.config/nvim/lua/options.lua
+make_link $CURRENT_DIR/.config/nvim ~/.config/nvim
 
-mkdir -p ~/.config/wezterm
-ln -s $CURRENT_DIR/.config/wezterm/keybinds.lua ~/.config/wezterm/keybinds.lua
-ln -s $CURRENT_DIR/.config/wezterm/mousebinds.lua ~/.config/wezterm/mousebinds.lua
-ln -s $CURRENT_DIR/.config/wezterm/on.lua ~/.config/wezterm/on.lua
-ln -s $CURRENT_DIR/.config/wezterm/utils.lua ~/.config/wezterm/utils.lua
-ln -s $CURRENT_DIR/.config/wezterm/wezterm.lua ~/.config/wezterm/wezterm.lua
-# colorscheme
-mkdir -p ~/.config/wezterm/lua
-ln -s $CURRENT_DIR/.config/wezterm/lua/rose-pine-dawn.lua ~/.config/wezterm/lua/rose-pine-dawn.lua
-ln -s $CURRENT_DIR/.config/wezterm/lua/rose-pine-moon.lua ~/.config/wezterm/lua/rose-pine-moon.lua
-ln -s $CURRENT_DIR/.config/wezterm/lua/rose-pine.lua ~/.config/wezterm/lua/rose-pine.lua
-ln -s $CURRENT_DIR/.config/wezterm/lua/aardvark_blue.lua ~/.config/wezterm/lua/aardvark_blue.lua
+make_link $CURRENT_DIR/.config/wezterm ~/.config/wezterm
 
-ln -sf $CURRENT_DIR/.zshrc ~/.zshrc
+make_link $CURRENT_DIR/.zshrc ~/.zshrc
+
