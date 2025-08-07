27bbc4761780d7430dcb92904a59273d989977de
Kaito Oshiro <k.oshiro@churadata.okinawa>
2025-08-07 16:48:39 +0900

    add: eza, batを追加

--- dot_zsh/aliases.zsh
+++ dot_zsh/aliases.zsh
@@ -3,3 +3,11 @@
 alias myip="curl http://ipecho.net/plain; echo"
 alias vi=nvim
 alias lg=lazygit
+alias ls='eza'
+alias la='eza -Glah'
+alias lt='eza -T'
+alias fc='cd "$(find . -type d | sk)"'
+alias vi='nvim'
+alias o='open'
+alias less="bat --theme='Monokai Extended Bright' --paging always"
+alias cat="bat --theme='Monokai Extended Bright'"
