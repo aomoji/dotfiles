863296b1800c5d16863ba4ad22e7b521f3761f5f
Kaito Oshiro <k.oshiro@churadata.okinawa>
2025-08-08 16:29:29 +0900

    add: lspを追加

--- dot_config/nvim/lua/configs/lspconfig.lua
+++ dot_config/nvim/lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
+local servers = { "pyright", "lua_ls", "rust_analyzer", "sqlls", "terraform_lsp", "tflint", "bashls" }
 vim.lsp.enable(servers)
 
--- read :h vim.lsp.config for changing options of lsp servers 
+-- read :h vim.lsp.config for changing options of lsp servers
