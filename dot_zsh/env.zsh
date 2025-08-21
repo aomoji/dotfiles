4a36ac8c39a6d0ae6534ac8a5d056e13706f6b6a
aomoji <ixia.laevigata@gmail.com>
2025-08-22 01:27:33 +0900

    fix: 無効なパスを削除し、代わりにchezmoiが使えるように変更

--- dot_zsh/env.zsh
+++ dot_zsh/env.zsh
@@ -1,6 +1,6 @@
 # 使用するシェルを zsh に指定
 export SHELL=$(command -v zsh)
 # PATHの追加
-export PATH="/root/.cargo/bin:$PATH"
+export PATH=""$HOME/.local/bin:$PATH""
 # ctrl+wで1つ前のスラッシュまで削除する
 export WORDCHARS='*?_.[]~-=&;!#$%^(){}<>'
